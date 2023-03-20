0f698f19b62148a2c71ce37df5141481f73e80ce
FatmaGh <fatma.ghaarsallah@gmail.com>
2023-03-20 16:09:22 +0100

    still fixing bugs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import './App.css';
-import Header from './components/Header/Header';
-import Home from './components/Home/Home';
-import About2 from './components/About/About2';
-import Portfolio from './components/Portfolio/Portfolio';
-import Resume from './components/Resume/Resume';
+import "./App.css";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import About2 from "./components/About/About2";
+import Portfolio from "./components/Portfolio/Portfolio";
+import Resume from "./components/Resume/Resume";
 // import Contact from './components/Home/Contact';
-import Footer from './components/Footer/Footer';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Footer from "./components/Footer/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      <BrowserRouter>
         <Header />
-        <Route exact path='/'  render={() => <Home/>} />
-        <Route exact path='/about'  render={() => <About2/>} />
-        <Route exact path='/resume'  render={() => <Resume/>} />
-        <Route exact path='/portfolio'  render={() => <Portfolio/>} />
-        <Route path='/' component={Footer}/>
-      </Router>
+        <Routes>
+          <Route exact path="/" render={() => <Home />} />
+          <Route exact path="/about" render={() => <About2 />} />
+          <Route exact path="/resume" render={() => <Resume />} />
+          <Route exact path="/portfolio" render={() => <Portfolio />} />
+          <Route path="/" component={Footer} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
